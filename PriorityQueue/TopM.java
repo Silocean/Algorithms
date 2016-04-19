523e456249adaa46bf31f4bbc50b504a056263cd
Yunfei Si <silenceocean@live.com>
2016-04-19 20:29:34 +0800

    add priorityqueue

---
+++ PriorityQueue/TopM.java
@@ -0,0 +1,29 @@
+package PriorityQueue;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * Created by Silocean on 2016-03-25.
+ */
+public class TopM {
+    public static void main(String[] args) {
+        int M = 3;
+        MaxPriorityQueue pq = new MaxPriorityQueue(10);
+        Scanner scanner = new Scanner(System.in);
+        String str = "";
+        while (!(str = scanner.nextLine()).equals("")) {
+            pq.insert(str);
+            if (pq.size() > M) {
+                pq.deleteMax();
+            }
+        }
+        Stack<String> stack = new Stack<>();
+        while (!pq.isEmpty()) {
+            stack.push((String) pq.deleteMax());
+        }
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+}
