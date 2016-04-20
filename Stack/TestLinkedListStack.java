5045b71f3274ad38755e13968a9f0e71750a7a7a
Yunfei Si <silenceocean@live.com>
2016-04-20 15:31:52 +0800

    add LinkedListStack

---
+++ Stack/TestLinkedListStack.java
@@ -0,0 +1,21 @@
+package Stack;
+
+import java.util.Iterator;
+
+/**
+ * Created by Silocean on 2016-04-20.
+ */
+public class TestLinkedListStack {
+    public static void main(String[] args) {
+        LinkedListStack<Integer> stack = new LinkedListStack<>();
+        stack.push(4);
+        stack.push(6);
+        stack.pop();
+        stack.push(1);
+        stack.push(3);
+        Iterator<Integer> iterator = stack.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
