db86d03ccb003c90fa25aaee71c366e6a523a647
Yunfei Si <silenceocean@live.com>
2016-04-21 11:21:42 +0800

    add Cycle
    这里书上所说无环图并不明确，

---
+++ Graph/Cycle.java
@@ -0,0 +1,33 @@
+package Graph;
+
+/**
+ * Created by Silocean on 2016-04-21.
+ */
+public class Cycle {
+    private boolean[] marked;
+    private boolean hasCycle;
+
+    public Cycle(Graph graph) {
+        marked = new boolean[graph.V()];
+        for (int s = 0; s < graph.V(); s++) {
+            if (!marked[s]) {
+                dfs(graph, s, s);
+            }
+        }
+    }
+
+    private void dfs(Graph graph, int v, int u) {
+        marked[v] = true;
+        for (int w : graph.adj(v)) {
+            if (!marked[w]) {
+                dfs(graph, w, v);
+            } else if (w != u) {
+                hasCycle = true;
+            }
+        }
+    }
+
+    public boolean hasCycle() {
+        return hasCycle;
+    }
+}
---
+++ Graph/TestCycle.java
@@ -0,0 +1,17 @@
+package Graph;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by Silocean on 2016-04-21.
+ */
+public class TestCycle {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("src/Graph/G.txt"));
+        Graph graph = new Graph(br);
+        Cycle cycle = new Cycle(graph);
+        System.out.println(cycle.hasCycle());
+    }
+}
