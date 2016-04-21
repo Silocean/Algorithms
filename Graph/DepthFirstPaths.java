5be9a2d2cf36312f51e1cf7f0bbd0e0373374772
Yunfei Si <silenceocean@live.com>
2016-04-21 09:46:50 +0800

    add DepthFirstPaths

---
+++ Graph/DepthFirstPaths.java
@@ -0,0 +1,54 @@
+package Graph;
+
+/**
+ * 使用深度优先搜索查找图中的路径
+ * Created by Silocean on 2016-04-21.
+ */
+public class DepthFirstPaths {
+    private boolean[] marked; // 这个顶点是否被访问过了
+    private int[] edgeTo; // 从起点到一个顶点的已知路径上的最后一个顶点
+    private final int s; // 起点
+
+    public DepthFirstPaths(Graph graph, int s) {
+        marked = new boolean[graph.V()];
+        edgeTo = new int[graph.V()];
+        this.s = s;
+        dfs(graph, s);
+    }
+
+    private void dfs(Graph graph, int v) {
+        marked[v] = true;
+        for (int w : graph.adj(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(graph, w);
+            }
+        }
+    }
+
+    /**
+     * 是否存在从s到v的路径
+     *
+     * @param v
+     * @return
+     */
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+    /**
+     * s到v的路径，如果不存在返回null
+     *
+     * @param v
+     * @return
+     */
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v)) return null;
+        LinkedListStack<Integer> paths = new LinkedListStack<Integer>();
+        for (int x = v; x != s; x = edgeTo[x]) {
+            paths.push(x);
+        }
+        paths.push(s);
+        return paths;
+    }
+}
