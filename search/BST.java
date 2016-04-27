17ab5090df60721ed70900e7627c4ee8836e41ab
Yunfei Si <silenceocean@live.com>
2016-04-27 17:16:49 +0800

    update BST

--- search/BST.java
+++ search/BST.java
@@ -1,68 +1,171 @@
 package search;
 
+import queue.Queue;
+
 /**
  * 二叉查找树
  * Created by Silocean on 2016-03-17.
  */
-public class BST {
+public class BST<Key extends Comparable<Key>, Value> {
 
     private Node root;
 
     private class Node {
-        public Node(int key) {
-            this.key = key;
-        }
-
-        private int key;
+        private Key key;
+        private Value value;
         private Node left, right;
-    }
+        private int N; // 以该节点为根的子树中的节点总数
 
-    /**
-     * 插入
-     *
-     * @param key
-     */
-    public void put(int key) {
-        root = this.put(root, key);
-    }
-
-    private Node put(Node node, int key) {
-        if (node == null) return new Node(key);
-        if (key < node.key) node.left = put(node.left, key);
-        else if (key > node.key) node.right = put(node.right, key);
-        return node;
-    }
-
-    /**
-     * 查找
-     *
-     * @param key
-     * @return
-     */
-    public Node get(int key) {
-        return this.get(root, key);
-    }
-
-    private Node get(Node node, int key) {
-        if (node == null) return null;
-        if (key < node.key) return get(node.left, key);
-        else if (key > node.key) return get(node.right, key);
-        return node;
-    }
-
-    /**
-     * 顺序打印所有节点
-     */
-    public void printAll() {
-        this.print(root);
-    }
-
-    private void print(Node node) {
-        if (node != null) {
-            print(node.left);
-            System.out.print(node.key + " ");
-            print(node.right);
+        public Node(Key key, Value value, int N) {
+            this.key = key;
+            this.value = value;
+            this.N = N;
         }
     }
+
+    public int size() {
+        return size(root);
+    }
+
+    public int size(Node x) {
+        if (x == null) return 0;
+        else return x.N;
+    }
+
+    public Value get(Key key) {
+        return get(root, key);
+    }
+
+    public Value get(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return get(x.left, key);
+        else if (cmp > 0) return get(x.right, key);
+        else return x.value;
+    }
+
+    public void put(Key key, Value value) {
+        root = put(root, key, value);
+    }
+
+    public Node put(Node x, Key key, Value value) {
+        if (x == null) return new Node(key, value, 1);
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = put(x.left, key, value);
+        else if (cmp > 0) x.right = put(x.right, key, value);
+        else x.value = value;
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public Key min() {
+        return min(root).key;
+    }
+
+    public Node min(Node x) {
+        if (x.left == null) return x;
+        else return min(x.left);
+    }
+
+    public Key max() {
+        return max(root).key;
+    }
+
+    public Node max(Node x) {
+        if (x.right == null) return x;
+        else return max(x.right);
+    }
+
+    public Key floor(Key key) {
+        Node x = floor(root, key);
+        if (x == null) return null;
+        else return x.key;
+    }
+
+    public Node floor(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) return x;
+        if (cmp < 0) return floor(x.left, key);
+        Node t = floor(x.right, key);
+        if (t != null) return t;
+        else return x;
+    }
+
+    public Key select(int k) {
+        return select(root, k).key;
+    }
+
+    public Node select(Node x, int k) {
+        if (x == null) return null;
+        int t = size(x.left);
+        if (t > k) return select(x.left, k);
+        else if (t < k) return select(x.right, k);
+        else return x;
+    }
+
+    public int rank(Key key) {
+        return rank(root, key);
+    }
+
+    public int rank(Node x, Key key) {
+        if (x == null) return 0;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return rank(x.left, key);
+        else if (cmp > 0) return rank(x.right, key) + size(x.left) + 1;
+        else return size(x.left);
+    }
+
+    public void deleteMin() {
+        root = deleteMin(root);
+    }
+
+    public Node deleteMin(Node x) {
+        if (x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public void delete(Key key) {
+        root = delete(root, key);
+    }
+
+    public Node delete(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = delete(x.left, key);
+        else if (cmp > 0) x.right = delete(x.right, key);
+        else {
+            if (x.right == null) return x.left;
+            if (x.left == null) return x.right;
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public Iterable<Key> keys() {
+        return keys(min(), max());
+    }
+
+    public Iterable<Key> keys(Key lo, Key hi) {
+        Queue<Key> queue = new Queue<>();
+        keys(root, queue, lo, hi);
+        return queue;
+    }
+
+    public void keys(Node x, Queue<Key> queue, Key lo, Key hi) {
+        if (x == null) return;
+        int cmplo = lo.compareTo(x.key);
+        int cmphi = hi.compareTo(x.key);
+        if (cmplo < 0) keys(x.left, queue, lo, hi);
+        if (cmplo <= 0 && cmphi >= 0) queue.enqueue(x.key);
+        if (cmphi > 0) keys(x.right, queue, lo, hi);
+    }
+
 
 }
--- test/TestBST.java
+++ test/TestBST.java
@@ -1,6 +1,8 @@
 package test;
 
-import bst.BST;
+import search.BST;
+
+import java.util.Iterator;
 
 /**
  * Created by Silocean on 2016-03-14.
@@ -8,10 +10,24 @@ import bst.BST;
 public class TestBST {
     public static void main(String[] args) {
         BST bst = new BST();
-        int[] arr = new int[]{2, 6, 1, 3, 9, 23, 0};
-        for (int i = 0; i < arr.length; i++) {
-            bst.put(arr[i]);
+        bst.put("a", 1);
+        bst.put("b", 2);
+        bst.put("c", 3);
+        bst.put("d", 4);
+        bst.put("e", 5);
+        bst.put("f", 6);
+        bst.put("g", 7);
+        // System.out.println(bst.min());
+        Iterator iterator = bst.keys().iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
         }
-        bst.printAll();
+        System.out.println();
+        System.out.println("========================");
+        bst.delete("c");
+        iterator = bst.keys().iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
     }
 }
