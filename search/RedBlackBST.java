8a66118f171110f6929d1ac98dfa3efe8e9191da
Yunfei Si <silenceocean@live.com>
2016-05-19 19:29:58 +0800

    add RedBlackBST

---
+++ search/RedBlackBST.java
@@ -0,0 +1,92 @@
+package search;
+
+/**
+ * Created by Silocean on 2016-05-19.
+ */
+public class RedBlackBST<Key extends Comparable<Key>, Value> {
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+
+    private Node root;
+
+    private class Node {
+        Key key; // 键
+        Value value; // 值
+        Node left, right; // 左右子树
+        int N; // 这棵子树中的节点总数
+        boolean color; // 由其父节点指向它的链接的颜色
+
+        Node(Key key, Value value, int N, boolean color) {
+            this.key = key;
+            this.value = value;
+            this.N = N;
+            this.color = color;
+        }
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) return 0;
+        else return x.N;
+    }
+
+    private boolean isRed(Node x) {
+        if (x == null) return false;
+        return x.color == RED;
+    }
+
+
+    private Node rotateLeft(Node h) {
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = h.color;
+        h.color = RED;
+        x.N = h.N;
+        h.N = size(h.left) + size(h.right) + 1;
+        return x;
+    }
+
+    private Node rotateRight(Node h) {
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+        x.color = h.color;
+        h.color = RED;
+        x.N = h.N;
+        h.N = size(h.left) + size(h.right) + 1;
+        return x;
+    }
+
+    private void flipColors(Node h) {
+        h.color = RED;
+        h.left.color = BLACK;
+        h.right.color = BLACK;
+    }
+
+    public void put(Key key, Value value) {
+        root = put(root, key, value);
+        root.color = BLACK;
+    }
+
+    private Node put(Node h, Key key, Value value) {
+        if (h == null) return new Node(key, value, 1, RED);
+
+        int cmp = key.compareTo(h.key);
+        if (cmp < 0) h.left = put(h.left, key, value);
+        if (cmp > 0) h.right = put(h.right, key, value);
+        else h.value = value;
+
+        if (!isRed(h.left) && isRed(h.right)) h = rotateLeft(h);
+        if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
+        if (isRed(h.left) && isRed(h.right)) flipColors(h);
+
+        h.N = size(h.left) + size(h.right) + 1;
+        return h;
+    }
+
+
+}
