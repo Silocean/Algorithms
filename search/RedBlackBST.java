bf854e2e26a5765fe9c8853b3cd9beb52e731175
Yunfei Si <silenceocean@live.com>
2016-05-20 11:38:30 +0800

    update RedBlackBST

--- search/RedBlackBST.java
+++ search/RedBlackBST.java
@@ -33,6 +33,10 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
         else return x.N;
     }
 
+    public boolean isEmpty() {
+        return root == null;
+    }
+
     private boolean isRed(Node x) {
         if (x == null) return false;
         return x.color == RED;
@@ -88,5 +92,149 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
         return h;
     }
 
+    /**
+     * 假设节点h为红色，h.left和h.left.left都是黑色，
+     * 将h.left或者h.left的子节点之一变红
+     *
+     * @param h
+     * @return
+     */
+    private Node moveRedLeft(Node h) {
+        flipColors(h);
+        if (isRed(h.right.left)) {
+            h.right = rotateRight(h.right);
+            h = rotateLeft(h);
+        }
+        return h;
+    }
+
+    public void deleteMin() {
+        if (!isRed(root.left) && !isRed(root.right)) {
+            root.color = RED;
+        }
+        root = deleteMin(root);
+        if (!isEmpty()) {
+            root.color = BLACK;
+        }
+    }
+
+    private Node deleteMin(Node h) {
+        if (h.left == null) return null;
+        if (!isRed(h.left) && !isRed(h.left.left)) {
+            h = moveRedLeft(h);
+        }
+        h.left = deleteMin(h.left);
+        return balance(h);
+    }
+
+    private Node balance(Node h) {
+        if (isRed(h.right)) h = rotateLeft(h);
+
+        if (!isRed(h.left) && isRed(h.right)) h = rotateLeft(h);
+        if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
+        if (isRed(h.left) && isRed(h.right)) flipColors(h);
+
+        h.N = size(h.left) + size(h.right) + 1;
+        return h;
+    }
+
+    /**
+     * 假设节点h为红色，h.right和h.right.left都是黑色，
+     * 将h.right或者h.right的子节点之一变红
+     *
+     * @param h
+     * @return
+     */
+    private Node moveRedRight(Node h) {
+        flipColors(h);
+        if (!isRed(h.left.left)) {
+            h = rotateRight(h);
+        }
+        return h;
+    }
+
+    public void deleteMax() {
+        if (!isRed(root.left) && isRed(root.right)) {
+            root.color = RED;
+        }
+        root = deleteMax(root);
+        if (!isEmpty()) {
+            root.color = BLACK;
+        }
+    }
+
+    private Node deleteMax(Node h) {
+        if (isRed(h.left)) {
+            h = rotateRight(h);
+        }
+        if (h.right == null) return null;
+        if (!isRed(h.right) && !isRed(h.right.left)) {
+            h = moveRedRight(h);
+        }
+        h.right = deleteMax(h.right);
+        return balance(h);
+    }
+
+    public void delete(Key key) {
+        if (!isRed(root.left) && isRed(root.right)) {
+            root.color = RED;
+        }
+        root = delete(root, key);
+        if (isEmpty()) {
+            root.color = BLACK;
+        }
+    }
+
+    private Node delete(Node h, Key key) {
+        if (key.compareTo(h.key) < 0) {
+            if (!isRed(h.left) && !isRed(h.left.left)) {
+                h = moveRedLeft(h);
+            }
+            h.left = delete(h.left, key);
+        } else {
+            if (isRed(h.left)) {
+                h = rotateRight(h);
+            }
+            if (key.compareTo(h.key) == 0 && h.right == null) {
+                return null;
+            }
+            if (!isRed(h.right) && !isRed(h.right.left)) {
+                h = moveRedRight(h);
+            }
+            if (key.compareTo(h.key) == 0) {
+                h.value = get(h.right, min(h.right).key);
+                h.key = min(h.right).key;
+                h.right = deleteMin(h.right);
+            } else {
+                h.right = delete(h.right, key);
+            }
+        }
+        return balance(h);
+    }
+
+    public Value get(Key key) {
+        if (key == null) return null;
+        return get(root, key);
+    }
+
+    private Value get(Node x, Key key) {
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+            if (cmp < 0) x = x.left;
+            else if (cmp > 0) x = x.right;
+            else return x.value;
+        }
+        return null;
+    }
+
+    public Key min() {
+        if (isEmpty()) return null;
+        return min(root).key;
+    }
+
+    private Node min(Node x) {
+        if (x.left == null) return x;
+        else return min(x.left);
+    }
 
 }
