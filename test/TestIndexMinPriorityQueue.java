de5750ac95f78a48cd4790e356e45646ae1df930
Silocean <silenceocean@live.com>
2016-10-26 19:23:21 +0800

    u

--- test/TestIndexMinPriorityQueue.java
+++ test/TestIndexMinPriorityQueue.java
@@ -9,7 +9,7 @@ public class TestIndexMinPriorityQueue {
     public static void main(String[] args) {
         IndexMinPriorityQueue<String> queue = new IndexMinPriorityQueue<>(5);
         queue.insert(0, "nihao");
-        queue.insert(1, "siyunfei");
+        queue.insert(1, "abj");
         queue.insert(2, "hello");
         queue.insert(3, "world");
         queue.insert(4, "bread");
